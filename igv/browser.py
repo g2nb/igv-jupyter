2b74bf4f7448b9fa34f9fada3afad14be2d5d993
Jim Robinson <jrobinso@broadinstitute.org>
2019-01-02 14:54:28 -0800

    Add docstrings

--- igv/browser.py
+++ igv/browser.py
@@ -8,6 +8,14 @@ class Browser:
 
     # Always remember the *self* argument
     def __init__(self, config):
+        """Initialize an igv Browser object.  See https://github.com/igvteam/igv.js/wiki/Browser-Configuration-2.0
+        for configuration options.
+
+        Arguments:
+            config: A dictionary specifying the browser configuration.  This will be converted to JSON and passed
+                    to igv.js  "igv.createBrowser(config)" as described in the igv.js documentation.
+        """
+
         id = self._gen_id()
         config["id"] = id
         self.igv_id = id
@@ -36,6 +44,9 @@ class Browser:
 
 
     def show(self):
+        """
+        Create an igv.js "Browser" instance on the front end.
+        """
         display(HTML("""<div id="%s" class="igv-js"></div>""" % (self.igv_id)))
         # DON'T check status before showing browser,
         msg = json.dumps({
@@ -46,6 +57,11 @@ class Browser:
         self.comm.send(msg)
 
     def search(self, locus):
+        """
+        Go to the specified locus.
+        :param locus: String of the form  "chromsosome:start-end", or for supported genomes a gene name.
+        """
+
         return self._send({
             "id": self.igv_id,
             "command": "search",
@@ -53,18 +69,28 @@ class Browser:
         })
 
     def zoom_in(self):
+        """
+        Zoom in by a factor of 2
+        """
         return self._send({
             "id": self.igv_id,
             "command": "zoomIn"
         })
 
     def zoom_out(self):
+        """
+        Zoom out by a factor of 2
+        """
         return self._send({
             "id": self.igv_id,
             "command": "zoomOut"
         })
 
     def load_track(self, track):
+        """
+        Load a track.  Corresponds to the igv.js Browser function loadTrack (see https://github.com/igvteam/igv.js/wiki/Browser-Control-2.0#loadtrack).
+        :param track: A dictionary specifying track options.  See https://github.com/igvteam/igv.js/wiki/Tracks-2.0.
+        """
         return self._send({
             "id": self.igv_id,
             "command": "loadTrack",
@@ -72,6 +98,12 @@ class Browser:
         })
 
     def on(self, eventName, cb):
+        """
+        Subscribe to an igv.js event.
+        :param eventName: Name of the event.  Currently only "locuschange" is supported.
+        :param cb: A callback function taking a single argument.  For the locuschange event this argument will contain
+                   a dictionary of the form  {chr, start, end}
+        """
         self.eventHandlers[eventName] = cb
         return self._send({
             "id": self.igv_id,
@@ -80,6 +112,10 @@ class Browser:
         })
 
     def remove(self):
+        """
+        Remove the igv.js Browser instance from the front end.  The browser object should be disposed of after calling
+        this method.
+        """
         return self._send({
             "id": self.igv_id,
             "command": "remove"
