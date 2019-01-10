8a4e4567db96240fbb9b77aa2d6e0a98557fa368
Douglass Turner <douglass.turner@gmail.com>
2019-01-10 10:28:31 -0500

    update

--- dev_install.sh
+++ dev_install.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
+set -o verbose
+
+rm -fr /usr/local/share/jupyter/
+rm -fr dist
+rm -fr build
+rm -fr igv.egg-info
+
 python setup.py install
 pip install -e .
+
 jupyter nbextension install --py igv
 jupyter nbextension enable --py igv
