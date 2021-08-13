705434c17d3c7cfc40c5b3351de0c8db6af1bf50
tmtabor <tmtabor@cloud.ucsd.edu>
2021-08-13 13:41:07 -0700

    Unifying Jupyter Notebook and JupyterLab code paths, initial menu work

--- igv/browser.py
+++ igv/browser.py
@@ -4,10 +4,11 @@ import json
 import random
 import time
 
+
 class _IGVComm:
+    """Wrapper class the for comm - the channel of communication between Jupyter's client-side and kernel-side"""
 
     def __init__(self, id):
-
         # Use comm to send a message from the kernel
         self.comm = Comm(target_name=id, data={})
 
@@ -16,10 +17,10 @@ class _IGVComm:
 
 
 class Browser(object):
+    """Object representing the igv.js browser"""
 
-    # Always remember the *self* argument
     def __init__(self, config):
-        """Initialize an igv Browser object.
+        """Initialize the igv browser object.
 
         :param: config - A dictionary specifying the browser configuration.  This will be converted to JSON and passed
                 to igv.js  "igv.createBrowser(config)" as described in the igv.js documentation. See
@@ -28,13 +29,11 @@ class Browser(object):
         """
 
         # Check for minimal igv.js requirements (the only required field for all tracks is url, which must be a string)
-        if isinstance(config, dict) == False:
-            raise Exception("config parameter must be a dictionary")
+        if not isinstance(config, dict): raise Exception("config parameter must be a dictionary")
         has_genome = "genome" in config or "reference" in config
-        if not has_genome:
-            raise Exception("config must specify either 'genome' or 'reference'")
+        if not has_genome: raise Exception("config must specify either 'genome' or 'reference'")
 
-
+        # Initialize browser object properties
         id = self._gen_id()
         config["id"] = id
         self.igv_id = id
@@ -46,23 +45,19 @@ class Browser(object):
         self.svg = None
         self.message_queue = []
 
-        # Add a callback for received messages.
+        # Add a callback for received messages
         @self.comm.comm.on_msg
         def _recv(msg):
             data = json.loads(msg['content']['data'])
-            if 'status' in data:
-                self.status = data['status']
-            elif 'locus' in data:
-                self.locus = data['locus']
-            elif 'svg' in data:
-                self.svg = data['svg']
+            if 'status' in data: self.status = data['status']
+            elif 'locus' in data: self.locus = data['locus']
+            elif 'svg' in data: self.svg = data['svg']
             elif 'event' in data:
                 if data['event'] in self.eventHandlers:
                     handler = self.eventHandlers[data['event']]
-                    eventData = None
-                    if 'data' in data:
-                        eventData = data['data']
-                    handler(eventData)
+                    event_data = None
+                    if 'data' in data: event_data = data['data']
+                    handler(event_data)
 
     @property
     def status(self):
@@ -76,10 +71,9 @@ class Browser(object):
             self._send(self.message_queue.pop(0))
 
     def show(self):
-        """
-        Create an igv.js "Browser" instance on the front end.
-        """
-        display(HTML("""<div id="%s"></div>""" % (self.igv_id)))
+        """Create an igv.js "Browser" instance on the front end."""
+        display(HTML(f"""<div class="igv-navbar"></div><div id="{self.igv_id}"></div>"""))
+
         # DON'T check status before showing browser,
         msg = json.dumps({
             "id": self.igv_id,
@@ -94,9 +88,7 @@ class Browser(object):
 
         :param locus:  Chromosome location of the form  "chromsosome:start-end", or for supported genomes (hg38, hg19, and mm10) a gene name.
         :type str
-
         """
-
         self._send({
             "id": self.igv_id,
             "command": "search",
@@ -107,7 +99,6 @@ class Browser(object):
         """
         Zoom in by a factor of 2
         """
-
         self._send({
             "id": self.igv_id,
             "command": "zoomIn"
@@ -131,8 +122,7 @@ class Browser(object):
         """
 
         # Check for minimal igv.js requirements (the only required field for all tracks is url, which must be a string)
-        if isinstance(track, dict) == False:
-            raise Exception("track parameter must be a dictionary")
+        if not isinstance(track, dict): raise Exception("track parameter must be a dictionary")
 
         self._send({
             "id": self.igv_id,
@@ -144,8 +134,8 @@ class Browser(object):
         """
         Fetch the current IGV view as an SVG image and display it in this cell
         """
-        div_id = self._gen_id();
-        display(HTML("""<div id="%s"></div>""" % div_id))
+        div_id = self._gen_id()
+        display(HTML(f"""<div id="{div_id}"></div>"""))
         self._send({
             "id": self.igv_id,
             "div": div_id,
@@ -166,14 +156,9 @@ class Browser(object):
         """
         Display the current SVG image.  You must call get_svg() before calling this method.
         """
-        if self.svg == None:
-            return "Must call get_svg() first"
-        elif self.svg == "FETCHING":
-            return 'Awaiting SVG - try again in a few seconds'
-        else:
-            svg = self.svg
-            self.svg == None
-            display(SVG(svg))
+        if self.svg is None: return "Must call get_svg() first"
+        elif self.svg == "FETCHING": return 'Awaiting SVG - try again in a few seconds'
+        else: display(SVG(self.svg))
 
     def roi(self, roi):
         """
@@ -185,21 +170,21 @@ class Browser(object):
             "roi": roi
         })
 
-    def on(self, eventName, cb):
+    def on(self, event_name, cb):
         """
         Subscribe to an igv.js event.
 
-        :param Name of the event.  Currently only "locuschange" is supported.
+        :param event_name - Name of the event.  Currently only "locuschange" is supported.
         :type str
         :param cb - callback function taking a single argument.  For the locuschange event this argument will contain
                 a dictionary of the form  {chr, start, end}
         :type function
         """
-        self.eventHandlers[eventName] = cb
+        self.eventHandlers[event_name] = cb
         self._send({
             "id": self.igv_id,
             "command": "on",
-            "eventName": eventName
+            "eventName": event_name
         })
 
     def remove(self):
@@ -219,5 +204,6 @@ class Browser(object):
         else:
             self.message_queue.append(msg)
 
-    def _gen_id(self):
+    @staticmethod
+    def _gen_id():
         return 'igv_' + str(random.randint(1, 10000000))
--- igv/nbextension/static/extension.js
+++ igv/nbextension/static/extension.js
@@ -1,225 +1,38 @@
-define(
-    ["nbextensions/igv/igvjs/igv"],
-    //["https://cdn.jsdelivr.net/npm/igv@2.1.0/dist/igv.min.js"],
-    function (igv) {
+// Entry point for the notebook bundle containing custom model definitions.
+//
+define(function () {
+    "use strict";
 
-        if (!igv.browserCache) {
-            igv.browserCache = {}
+    const base_path = document.querySelector('body').getAttribute('data-base-url') +
+        'nbextensions/igv/';
+
+    function load_css(url) {
+        const link = document.createElement("link");
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href = url;
+        document.getElementsByTagName("head")[0].appendChild(link);
+    }
+    // load_css(base_path + 'notebook.css');
+
+    window['requirejs'].config({
+        map: {
+            '*': {
+                '@igvteam/igv-jupyter': 'nbextensions/igv/index',
+            },
         }
-
-        /**
-         * Load the IGV.js nbextension
-         */
-        function load_ipython_extension() {
-            registerComm()
-        }
-
-        function registerComm() {
-
-            Jupyter.notebook.kernel.comm_manager.register_target('igvcomm',
-
-                function (comm, msg) {
-                    // comm is the frontend comm instance
-                    // msg is the comm_open message, which can carry data
-
-                    // Register handlers for later messages:
-                    comm.on_msg(function (msg) {
-                        var data = JSON.parse(msg.content.data)
-                        var method = data.command
-                        var id = data.id
-                        var browser = getBrowser(id)
-                        switch (method) {
-
-                            case "create":
-                                var div = document.getElementById(id)
-                                createBrowser(div, data.options, comm)
-                                break
-
-                            case "loadROI":
-                                loadROI(id, data.roi);
-                                break;
-
-                            case "loadTrack":
-                                loadTrack(id, data.track)
-                                break
-
-                            case "search":
-                                search(id, data.locus)
-                                break
-
-                            case "zoomIn":
-                                try {
-                                    browser.zoomIn()
-                                } catch (e) {
-                                    alert(e.message)
-                                    console.error(e)
-                                } finally {
-                                    comm.send('{"status": "ready"}')
-                                }
-                                break
-
-                            case "zoomOut":
-                                try {
-                                    browser.zoomOut()
-                                } catch (e) {
-                                    alert(e.message)
-                                    console.error(e)
-                                } finally {
-                                    comm.send('{"status": "ready"}')
-                                }
-                                break
-
-                            case "remove":
-                                try {
-                                    delete igv.browserCache[id]
-                                    var div = document.getElementById(id)
-                                    div.parentNode.removeChild(div)
-                                } catch (e) {
-                                    alert(e.message)
-                                    console.error(e)
-                                } finally {
-                                    comm.send('{"status": "ready"}')
-                                }
-                                break
-
-                            case "toSVG":
-                                try {
-                                    var svg = browser.toSVG()
-                                    var div = document.getElementById(data.div)
-                                    if(div) {
-                                        div.outerHTML += svg
-                                    }
-                                    comm.send(JSON.stringify({
-                                        "svg": svg
-                                    }))
-                                } catch (e) {
-                                    alert(e.message)
-                                    console.error(e)
-                                } finally {
-                                    comm.send('{"status": "ready"}')
-                                }
-                                break;
-
-                            case "on":
-                                try {
-                                    if ("locuschange" === data.eventName) {
-                                        browser.on(data.eventName, function (referenceFrame) {
-                                            comm.send(JSON.stringify({
-                                                "event": data.eventName,
-                                                "data": referenceFrame
-                                            }))
-                                        })
-                                    } else {
-                                        alert("Unsupported event: " + data.eventName)
-                                    }
-                                } catch (e) {
-                                    alert(e.message)
-                                    console.error(e)
-                                } finally {
-                                    comm.send('{"status": "ready"}')
-                                }
-                                break
-
-                            default:
-                                console.error("Unrecognized method: " + msg.method)
-                        }
-
-                        function getBrowser(id) {
-                            return igv.browserCache[id]
-                        }
-
-                        // ASYNC functino wrappers
-
-                        function createBrowser(div, config) {
-                            // TODO -- send message that browser is ready
-                            igv.createBrowser(div, config)
-                                .then(function (browser) {
-                                    igv.browserCache[config.id] = browser;
-                                    if (comm) {
-                                        comm.send('{"status": "ready"}')
-                                    }
-
-                                    // Uncomment to send locus change events to server object (browser).  This generates a lot of traffic.
-                                    //browser.on('locuschange', function (referenceFrame) {
-                                    //    comm.send(JSON.stringify({"locus": referenceFrame}))
-                                    //});
-                                })
-                                .catch(function (error) {
-                                    comm.send('{"status": "ready"}')
-                                    alert(error.message);
-                                    console.error(e)
-                                })
-                        }
-
-                        function is_local(url) {
-                            if (url.startsWith('data:')) return false;  // Special case for embedded data URLs
-                            const regexp = /(ftp|http|https|gs):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-                            return !regexp.test(url);
-                        }
-
-                        function download_url(url) {
-                            return `files/${url}`;
-                        }
-
-                        function prepare_urls(config) {
-                            const to_check = ['url', 'indexURL', 'fastaURL', 'cytobandURL'];
-                            for (const param of to_check) {
-                                const url = config[param];
-                                if (!url) return;  // If this param is not defined, skip
-                                if (is_local(url)) {
-                                    config[param] = download_url(url);
-                                }
-                            }
-                        }
-
-                        function loadROI(id, roi) {
-                            const browser = getBrowser(id);
-                            browser.loadROI([roi])
-                                .then(function (track) {
-                                    comm.send('{"status": "ready"}')
-                                })
-                                .catch(function (error) {
-                                    comm.send('{"status": "ready"}');
-                                    alert(error.message);
-                                    console.error(e)
-                                });
-                        }
-
-                        function loadTrack(id, config) {
-                            var browser = getBrowser(id)
-                            prepare_urls(config);
-                            config.sync = true
-                            browser.loadTrack(config)
-                                .then(function (track) {
-                                    comm.send('{"status": "ready"}')
-                                })
-                                .catch(function (error) {
-                                    comm.send('{"status": "ready"}')
-                                    alert(error.message);
-                                    console.error(e)
-                                })
-                        }
-
-                        function search(id, locus) {
-                            var browser = getBrowser(id);
-                            browser.search(locus)
-                                .then(function (ignore) {
-                                    comm.send('{"status": "ready"}')
-                                })
-                                .catch(function (error) {
-                                    comm.send('{"status": "ready"}')
-                                    alert(error.message)
-                                    console.error(e)
-                                })
-                        }
-                    });
-                    comm.on_close(function (msg) {
-                    });
-                });
-        }
-
-        return {
-            load_ipython_extension: load_ipython_extension,
-        };
-
     });
+
+    // Load the toolbox
+    require(['@igvteam/igv-jupyter'], function(igv) {
+        console.log('igv-jupyter loaded');
+        window.igv = igv;
+        console.log(igv);
+    });
+
+    // Export the required load_ipython_extension function
+    return {
+        load_ipython_extension: function () {
+        }
+    };
+});
